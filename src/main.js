7056dfdcd0a151a79eb6b0ed3fe2634faa63002f
sygo <seungyong@saltlux.com>
2021-08-03 23:13:36 +0900

    기본 라우터 만들어보기

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Login = { template: '<div>Login Page</div>'}
+
+const routes = {
+  '/': App,
+  '/login': Login,
+}
+
+new Vue({
+  el: '#app',
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || { template: '<div>Page not found</div>'}
+    }
+  },
+  render(h) {
+    return h(this.VueComponent);
+  }
+})
+
+// createApp(App).mount('#app')
