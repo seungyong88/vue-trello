6ff306d320a815f0b4292ad19e76be12c2e801b3
seungyong88 <gosng2001@naver.com>
2021-08-04 11:07:17 +0900

    nav component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-view />
+  <div class="app_body">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <NavBar />
+    <router-view />
+   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
 
 export default {
   name: 'App',
+  components: {
+    NavBar
+  }
 }
 </script>
+
+<style>
+  .app_body {
+    text-align: center;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link> | 
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
